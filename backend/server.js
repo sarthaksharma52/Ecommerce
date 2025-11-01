adefc0fb93df42e933d6b84081066a6f95cf6e31
Sarthak Sharma <sarthak.dec04@gmail.com>
2025-11-02 03:00:31 +0530

    allow frontend cors origin

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,12 @@ import orderRoutes from './routes/orders.js';
 dotenv.config();
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "*",
+    credentials: true
+  })
+);
 
 app.use(cookieParser());
 app.use(express.json());
